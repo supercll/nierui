a7d78941378a431e52dc64eff03382d6ea742672
supercll <1033581609@qq.com>
2023-01-30 16:34:32 +0800

    feat: dialog bg noise

--- src/lib/componets/Dialog.vue
+++ src/lib/componets/Dialog.vue
@@ -1,20 +1,18 @@
 <template>
   <Fragment v-if="visible">
     <Teleport to="body">
-      <div class="nier-dialog-overlay" @click="onClickOverlay"></div>
-      <div class="nier-dialog-wrapper">
-        <div class="nier-dialog">
-          <header>
-            <slot name="title" />
-            <span @click="close" class="nier-dialog-close"></span>
-          </header>
-          <main>
-            <slot name="content" />
-          </main>
-          <footer>
-            <n-button level="main" @click="sure">OK</n-button>
-            <n-button @click="cancel">Cancel</n-button>
-          </footer>
+      <div class="nier-dialog">
+        <div class="nier-dialog-overlay" @click="onClickOverlay"></div>
+        <div class="nier-dialog-wrapper">
+          <div @click="close" class="nier-dialog-close"></div>
+          <div class="nier-dialog-iframe"></div>
+          <div class="nier-dialog-inner">
+            <slot />
+            <footer>
+              <n-button level="main" @click="sure">OK</n-button>
+              <n-button @click="cancel">Cancel</n-button>
+            </footer>
+          </div>
         </div>
       </div>
     </Teleport>
@@ -26,6 +24,11 @@ import NButton from './Button.vue'
 export default {
   name: 'nr-dialog',
   props: {
+    size: {
+      type: String,
+      default: '60%',
+    },
+
     visible: {
       type: Boolean,
       default: false,
@@ -80,11 +83,64 @@ $radius: 4px;
 $border-color: #d9d9d9;
 
 .nier-dialog {
-  background: white;
-  border-radius: $radius;
-  box-shadow: 0 0 3px fade_out(black, 0.5);
-  min-width: 15em;
-  max-width: 90%;
+  &-inner {
+    background: none;
+    border-radius: $radius;
+    border: 1px solid #999;
+    // box-shadow: 0 0 3px fade_out(black, 0.5);
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    z-index: 3;
+
+    >footer {
+      border-top: 1px solid $border-color;
+      padding: 12px 16px;
+      text-align: right;
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+    }
+  }
+
+  &-iframe {
+    width: 100%;
+    height: 100%;
+    &::before {
+      display: block;
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      transform: scale(1,1);
+      background-image: url('https://nier.oss-cn-shanghai.aliyuncs.com/web/noise.png');
+      background-size: contain;
+      z-index: 2;
+    }
+    &::after {
+      display: block;
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-image: url('https://nier.oss-cn-shanghai.aliyuncs.com/web/noise.png');
+      background-size: contain;
+      transform: scale(1,-1);
+      transition-delay: 0s;
+      transition-timing-function: cubic-bezier(0.87,0,0.13,1);
+      animation-name: noise;
+      animation-duration: 0.5s;
+      animation-iteration-count: infinite;
+      animation-timing-function: steps(5);
+      z-index: 3;
+    }
+  }
 
   &-overlay {
     position: fixed;
@@ -102,51 +158,41 @@ $border-color: #d9d9d9;
     top: 50%;
     transform: translate(-50%, -50%);
     z-index: 11;
-  }
-
-  >header {
-    padding: 12px 16px;
-    border-bottom: 1px solid $border-color;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 20px;
-  }
-
-  >main {
-    padding: 12px 16px;
-  }
-
-  >footer {
-    border-top: 1px solid $border-color;
-    padding: 12px 16px;
-    text-align: right;
+    width: v-bind(size);
+    height: v-bind(size);
   }
 
   &-close {
-    position: relative;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: translateY(-200%);
     display: inline-block;
-    width: 16px;
-    height: 16px;
+    width: 32px;
+    height: 32px;
     cursor: pointer;
-
-    &::before,
-    &::after {
-      content: "";
-      position: absolute;
-      height: 1px;
-      background: black;
-      width: 100%;
-      top: 50%;
-      left: 50%;
+    background: url('https://nier.oss-cn-shanghai.aliyuncs.com/web/staff_close_pc.png');
+    background-size: contain;
+    background-color: rgba(255,255,255,0);
+    transition-property: background-color;
+    transition-duration: 0.25s;
+    &:hover {
+      background-color: rgba(255,255,255,0.2);
     }
 
-    &::before {
-      transform: translate(-50%, -50%) rotate(-45deg);
+  }
+
+  @keyframes noise {
+    0% {
+      opacity: 0
     }
 
-    &::after {
-      transform: translate(-50%, -50%) rotate(45deg);
+    50% {
+      opacity: 1
+    }
+
+    to {
+      opacity: 0
     }
   }
 }
